316f50bc076767d085c671d5d721daba67c917ae
Elkhan <elkhan.safarov10@yandex.ru>
2019-05-06 17:15:41 +0500

    Created App class. Created initial-data.js

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import initialData from './initial-data';
+
+class App extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.state = initialData;
+	}
+
+	render(){
+		return this.state.columnOrder.map((columnId) => {
+			const column = this.state.columns[columnId];
+			const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+
+			return column.title;
+		});
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => 'Hello world';
+import App from './App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
