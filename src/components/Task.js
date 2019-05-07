f618a93edca7c22b9b948ae0839c3df459463947
Elshad <safarov.elshad007@gmail.com>
2019-05-07 22:10:18 +0500

    Added allow Movement using react-beautiful-dnd Draggable and Droppable Props

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,20 +7,31 @@ const Container = styled.div`
     border-radius: 5px;
     padding: 8px;
     margin-bottom: 8px;
-    background-color: ${props => (props.isDragging ? '#AACD00' : '#fff')};
+    background-color: ${props =>
+        props.isDragDisabled
+            ? 'lightgrey'
+            : props.isDragging
+                ? 'lightgreen'
+                : 'white'};
     display: flex;
 `;
 
 class Task extends React.Component {
     render() {
+        const isDragDisabled = this.props.task.id === 'task-1';
         return (
-            <Draggable draggableId={this.props.task.id} index={this.props.index}>
+            <Draggable
+                draggableId={this.props.task.id}
+                index={this.props.index}
+                isDragDisabled={isDragDisabled}
+            >
                 {(provided, snapshot) => (
                     <Container
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
                         ref={provided.innerRef}
                         isDragging={snapshot.isDragging}
+                        isDragDisabled={isDragDisabled}
                     >
                         {this.props.task.content}
                     </Container>
