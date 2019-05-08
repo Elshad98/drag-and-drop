82d2db35bf001844ecd959283ca613c0724a217b
Elshad <safarov.elshad007@gmail.com>
2019-05-08 13:13:03 +0500

    Added optimiza Perfomance with shouldComponentUpdate and PureComponent

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ const Container = styled.div`
     display: flex;
 `;
 
+class InnerList extends React.PureComponent {
+    render() {
+        const { column, taskMap, index } = this.props;
+        const tasks = column.taskIds.map(taskId => taskMap[taskId]);
+        return <Column column={column} tasks={tasks} index={index} />
+    }
+}
+
 class App extends React.Component{
 	constructor(props){
 		super(props);
@@ -109,12 +117,11 @@ class App extends React.Component{
                         >
                             {this.state.columnOrder.map((columnId, index) => {
                                 const column = this.state.columns[columnId];
-                                const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
                                 return (
-                                    <Column
+                                    <InnerList
                                         key={column.id}
                                         column={column}
-                                        tasks={tasks}
+                                        taskMap={this.state.tasks}
                                         index={index}
                                     />);
                             })}
