069ac01b50a5b1896e28f72aaa7a6b54b63a88fb
Elshad98 <safarov.elshad007@gmail.com>
2021-01-10 14:28:16 +0500

    Updates

---
+++ precache-manifest.7b29afdd7a72368d39f7faaf95e6946d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78cfef761c0e312ba5bac9784a08bcda",
+    "url": "/drag-and-drop/index.html"
+  },
+  {
+    "revision": "9ba75a9a9f2e71328113",
+    "url": "/drag-and-drop/static/css/2.f366d23a.chunk.css"
+  },
+  {
+    "revision": "c9b966dc225cf7f24c5b",
+    "url": "/drag-and-drop/static/css/main.41f3f4e8.chunk.css"
+  },
+  {
+    "revision": "9ba75a9a9f2e71328113",
+    "url": "/drag-and-drop/static/js/2.d62d4a47.chunk.js"
+  },
+  {
+    "revision": "c9b966dc225cf7f24c5b",
+    "url": "/drag-and-drop/static/js/main.8fa72189.chunk.js"
+  },
+  {
+    "revision": "0eb25b5e18f0e2ded839",
+    "url": "/drag-and-drop/static/js/runtime~main.05b4015d.js"
+  },
+  {
+    "revision": "34218e29f0973d117658c62f62735ee5",
+    "url": "/drag-and-drop/static/media/princess.34218e29.png"
+  }
+]);
